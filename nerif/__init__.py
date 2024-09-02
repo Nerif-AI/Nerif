10d2702d68bb866870e6a311f87ded568147ce0f
Chivier Humber <chivier.humber@outlook.com>
2024-09-02 14:44:49 +0800

    fix: fix pypi package path

---
+++ nerif/__init__.py
@@ -0,0 +1,11 @@
+from . import nerif_core
+from . import nerif_agent
+from .nerif_agent import (
+    SimpleChatAgent,
+)
+from .nerif_core import (
+    Nerif,
+    NerifMatch,
+    nerif,
+    nerif_match,
+)
