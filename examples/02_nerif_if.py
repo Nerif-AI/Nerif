0da8d53915dd46c926aa09163c59650bab74fc1c
Chivier Humber <chivier.humber@outlook.com>
2024-09-25 15:38:10 +0800

    feat: prepare nerif example

---
+++ examples/02_nerif_if.py
@@ -0,0 +1,9 @@
+from nerif.nerif_core import nerif
+from nerif.nerif_agent import SimpleChatAgent
+
+agent = SimpleChatAgent()
+if nerif("the sky is blue"):
+    print("True")
+else:
+    print("No", end=", ")
+    print(agent.chat("what is the color of the sky?"))
