7a5e369d605b5cb9a016e7db8ba356c31aa2dd43
anyin233 <shizukuprprpr@gmail.com>
2024-09-26 16:18:32 +0800

    feat: Add nerif example for matching selections and NerifFormat

---
+++ examples/03_nerif_match.py
@@ -0,0 +1,13 @@
+from nerif.nerif_core import nerif_match_string
+from nerif.nerif_agent import SimpleChatAgent
+
+selections = [
+  "iPhone 5",
+  "iPhone 6",
+  "iPhone 12"
+]
+
+match nerif_match_string(selections=selections, text="Which iPhone is the most powerful one?"):
+  case 0: print("iPhone 5")
+  case 1: print("iPhone 6")
+  case 2: print("iPhone 12")
