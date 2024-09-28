1d42ed3d5f609ef9d75b27d160667a28039241e5
Erertertet <erertertet@gmail.com>
2024-09-28 16:58:43 -0500

    feat: more args for setup_log

--- nerif/nerif_core/log.py
+++ nerif/nerif_core/log.py
@@ -2,24 +2,29 @@ import logging
 import sys
 
 
-def set_up_logging(out="out.log", mode="a", fmt="%(levelname)s\t%(name)s\t%(message)s", std=False):
+def set_up_logging(
+    out_file: None | str = None, 
+    mode: str = "a", 
+    fmt: str = "%(levelname)s\t%(name)s\t%(message)s", 
+    std: bool = False,
+    level: int | str = logging.DEBUG
+):
 
     logger = logging.getLogger("Nerif")
-    logger.setLevel(logging.DEBUG)
-
-    # setting logging format
+    logger.setLevel(level)
 
     basic_formatting = logging.Formatter(fmt)
 
-    # logging into file
-    file_handler = logging.FileHandler(out, mode=mode)
-    file_handler.setFormatter(basic_formatting)
-    logger.addHandler(file_handler)
+    if out_file != None:
+        file_handler = logging.FileHandler(out_file, mode=mode)
+        file_handler.setFormatter(basic_formatting)
+        logger.addHandler(file_handler)
 
     if std:
         stream_handler = logging.StreamHandler(sys.stdout)
         stream_handler.setFormatter(basic_formatting)
         logger.addHandler(stream_handler)
-    
-    logger.info("-" * 20)
-    logger.info("logging enabled")
+
+    if std or out_file != None:
+        logger.info("-" * 20)
+        logger.info("logging enabled")
