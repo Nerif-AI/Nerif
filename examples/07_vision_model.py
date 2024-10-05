e39be9268b1cd7ad474987a83896b5a2da868392
Chivier Humber <chivier.humber@outlook.com>
2024-10-05 10:52:02 +0100

    chore: add example for vision model

---
+++ examples/07_vision_model.py
@@ -0,0 +1,12 @@
+import os
+import sys
+
+from nerif.nerif_agent import *
+from nerif.nerif_core import *
+
+if __name__ == '__main__':
+    vision_model = VisionAgent(model="openrouter/openai/gpt-4o-2024-08-06")
+    vision_model.append_message(nerif_agent.MessageType.IMAGE_URL, "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png")
+    vision_model.append_message(nerif_agent.MessageType.TEXT, "what is in this image?")
+    response = vision_model.chat()
+    print(response)
