e3a028cba54c0e180ae1f768ebbb12bfde4a40b2
anyin233 <shizukuprprpr@gmail.com>
2024-10-17 23:11:44 +0800

    merge: Removed unit test of NerifTokenCounter from nerif_agent_test.py (#18)
    * feat: add a universal API store class
    
    * feat: change the NerifTokenCounter to a local token counter
    
    * refactor: Modify NerifTokenCounter to support appending messages
    
    * refactor: Remove redundant code in token counter example
    
    * refactor: Remove unnecessary files from .gitignore
    
    * feat: integrate counter into SimpleChatAgent
    
    * refactor: integrate counter into SimpleEmbeddingAgent and LogitsAgent
    
    * test: add unit test for NerifTokenCounter
    
    * feat: add token counter into nerif and nerif_match
    
    * misc: add example for counter
    
    * feat: supported convert human readable list to python list
    
    * refactor: remove redundant code from NerifFormat
    
    * refactor: Add document for NerifTokenCounter
    
    * refactor: change the comment of NerifTokenCounter
    
    * refactor: moved my implementation of token counter to right folder
    
    * chore: format code with isort and black
    
    * feat: Counting token from the response of LLM providers
    
    * feat: change response parser based on model name
    
    * feat: integrate token counter into agents
    
    * refactor: rename filename of token counter and remove some unused file
    
    * refactor: export proper TokenCounter
    
    * feat: integrate token counter into nerif core
    
    * refactor: pretty print token counter
    
    * refactor: change the example of token counter
    
    * refactor: remove unused unit test
    
    * chore: code format
    
    * refactor: import nerif_match_string in nerif_test
    
    * chore: file format
    
    * chore: update lint cicd check
    
    * chore: update lint cicd check
    
    * refactor: remove unit test of token counter in nerif_agent_test.py
    
    * chore: file format
    
    ---------
    
    Co-authored-by: Yeqi Huang <chivier.humber@outlook.com>
    Co-authored-by: cydia2001 <cydia2001@duck.com>

--- test/nerif_agent_test.py
+++ test/nerif_agent_test.py
@@ -5,12 +5,7 @@ import warnings
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
-from nerif.agent import (
-    LogitsAgent,
-    NerifTokenCounter,
-    SimpleChatAgent,
-    SimpleEmbeddingAgent,
-)
+from nerif.agent import LogitsAgent, SimpleChatAgent
 
 
 class TestNerifAgent(unittest.TestCase):
@@ -38,31 +33,6 @@ class TestNerifAgent(unittest.TestCase):
         result1 = simple_agent1.chat("Generate a random number")
         result2 = simple_agent2.chat("Generate a random number")
         self.assertNotEqual(result1, result2)
-
-    def test_agent_counter(self):
-        simple_agent = SimpleChatAgent()
-        simple_agent.chat("Hello, how are you?")
-        self.assertEqual(len(simple_agent.counter.model_token), 1)
-        simple_agent.chat("Hello, how are you?")
-        self.assertEqual(len(simple_agent.counter.model_token), 2)
-        simple_agent.chat("Hello, how are you again?", append=True)
-        self.assertEqual(len(simple_agent.counter.model_token), 2)
-
-        simple_agent = SimpleEmbeddingAgent()
-        simple_agent.encode("Hello, how are you?")
-        self.assertEqual(len(simple_agent.counter.model_token), 1)
-
-        simple_agent = LogitsAgent()
-        simple_agent.chat("Hello, how are you?", max_tokens=1)
-        self.assertEqual(len(simple_agent.counter.model_token), 1)
-
-    def test_external_counter(self):
-        external_counter = NerifTokenCounter()
-        simple_agent1 = SimpleChatAgent(counter=external_counter)
-        simple_agent2 = SimpleChatAgent(counter=external_counter)
-        simple_agent1.chat("Hello, how are you?")
-        simple_agent2.chat("Hello, how are you?")
-        self.assertEqual(len(external_counter.model_token), 2)
 
     def test_logits_agent(self):
         logits_agent = LogitsAgent()
